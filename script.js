890ca02a8e7c64c044a97faae18392308547da8f
Nicholas Ward <npward@berkeley.edu>
2023-09-06 14:04:34 -0700

    functional

--- script.js
+++ script.js
@@ -2,5 +2,10 @@ fetch("https://159.223.199.153:3000/json")
   .then((response) => response.json())
   .then((json) => {
 	  console.log(json);
+    let phrase = json.phrase;
+    let reference = json.reference;
+
+    document.querySelector("#phrase").innerHTML = phrase;
+    document.querySelector("#reference").innerHTML = reference;
   }
 );
